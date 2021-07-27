046d0d7faa4a4de8c1d543977b380aada4af13e9
Michelle <brendamichelle995@gmail.com>
2021-07-27 16:07:44 -0400

    after toy tale review

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 let addToy = false;
+const newToyForm = document.querySelector('form.add-toy-form')
+const toyCollectionDiv = document.querySelector('div#toy-collection')
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +16,90 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+function renderOneToy(toy) {
+  const div = document.createElement('div')
+  div.classList.add('card')
+  div.dataset.id = toy.id
+
+  div.innerHTML = `
+          <h2>${toy.name}</h2>
+          <img src=${toy.image} class="toy-avatar" />
+          <p>${toy.likes} Likes </p>
+          <button class="like-btn" data-id=${toy.id}>Like <3</button>`
+
+
+  const toyCollectionDiv = document.querySelector('div#toy-collection')
+  toyCollectionDiv.append(div)
+}
+
+
+function renderAllToys() {
+  fetch('http://localhost:3000/toys')
+    .then(response => {
+      // console.log(response)
+      return response.json()
+    })
+    .then(toyArr => toyArr.forEach(renderOneToy))
+}
+
+
+
+
+
+newToyForm.addEventListener('submit', e => {
+  e.preventDefault()
+  // console.log(e.target)
+  // debugger
+
+  const nameInput = e.target.name.value
+  const imageInput = e.target.image.value
+
+  const configObject = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({ name: nameInput, image: imageInput, likes: 0 })
+  }
+
+  fetch('http://localhost:3000/toys', configObject)
+    .then(response => {
+      console.log(response)
+      return response.json()
+    })
+    .then(toy => {
+      renderOneToy(toy)
+    })
+
+})
+
+
+toyCollectionDiv.addEventListener('click', e => {
+  if (e.target.classList.contains('like-btn')) {
+    const pLikesDisplay = e.target.previousElementSibling
+    const oldLikes = parseInt(pLikesDisplay.textContent)
+    const newLikes = oldLikes + 1
+
+    const configObject = {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ likes: newLikes })
+    }
+
+    fetch(`http://localhost:3000/toys/${e.target.dataset.id}`, configObject)
+      .then(response => response.json())
+      .then(data => {
+        pLikesDisplay.textContent = `${data.likes} Likes`
+      })
+  }
+})
+
+
+
+/* APP INIT */
+renderAllToys()
